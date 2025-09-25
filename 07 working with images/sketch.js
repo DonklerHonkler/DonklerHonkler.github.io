da1cc798e3f9372c02132f8e28f2b7c00be05e1a
DonklerHonkler <2602644@spsd.sk.ca>
2025-09-25 12:18:13 -0600

    Demo in progress

--- 07 working with images/sketch.js
+++ 07 working with images/sketch.js
@@ -5,12 +5,39 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let lionL; let lionR;
+let facingRight = false
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  loadAssets();
+  imageMode(CENTER);
+  noCursor();
 }
+async function loadAssets() {
 
+  //handle loading all the images
+  lionL = await loadImage("assets/lion-left.png");
+  lionR = await loadImage("assets/lion-right.png")
+
+}
 function draw() {
   background(220);
-  print(mouseButton);  // CENTER LEFT RIGHT
+  //update our direction
+  if (pmouseX < mouseX) { //implies mouse is moving right
+    facingRight = true;
+  }
+  else if (pmouseX > mouseX) { //implies moving left
+    facingRight = false;
+  }
+
+
+  if (facingRight) {
+    image(lionR, mouseX, mouseY, lionR.width / 2, lionR.height / 2);
+
+  }
+  else {
+    image(lionL, mouseX, mouseY, lionL.width / 2, lionL.height / 2);
+  }
 }
+
